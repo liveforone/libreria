e2b920a4edee9a5eb72504460ff5de09e32e381f
liveforone <yc4852@gmail.com>
2022-10-27 10:38:55 +0900

    Good Commit

--- src/test/java/libreria/libreria/orders/service/OrderServiceTest.java
+++ src/test/java/libreria/libreria/orders/service/OrderServiceTest.java
@@ -2,6 +2,7 @@ package libreria.libreria.orders.service;
 
 import libreria.libreria.orders.model.Orders;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrderServiceTest {
 
     @Test
-    void getOrderDayTest() {
+    @DisplayName("주문 취소 날짜 테스트")
+    void getOrderAbleCancelDayTest() {
         //테스트 당시 날짜 기준으로 한참 전 날짜임
         LocalDate localDate = LocalDate.of(2022, 9, 8);
         int ableDate = localDate.getDayOfYear() + 7;  //생성날짜 + 7일
