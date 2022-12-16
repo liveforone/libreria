a533ab96cb3e888a995432f2bcab7056165e90d4
liveforone <yc4852@gmail.com>
2022-12-16 23:28:26 +0900

    Add OrderServiceTest and UserUtilsTest

--- src/test/java/libreria/libreria/orders/service/OrderServiceTest.java
+++ src/test/java/libreria/libreria/orders/service/OrderServiceTest.java
@@ -1,7 +1,5 @@
 package libreria.libreria.orders.service;
 
-import libreria.libreria.orders.model.Orders;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class OrderServiceTest {
---
+++ src/test/java/libreria/libreria/user/util/UserUtilsTest.java
@@ -0,0 +1,28 @@
+package libreria.libreria.user.util;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserUtilsTest {
+
+    @Test
+    @DisplayName("비밀번호 복호화 테스트")
+    void checkPasswordMatching() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        //문자열 상태
+        String inputPassword = "1234";
+
+        //암호화된 상태
+        String password = passwordEncoder.encode(inputPassword);
+
+        boolean matches = passwordEncoder.matches(inputPassword, password);
+        Assertions.assertThat(matches).isTrue();
+    }
+}
